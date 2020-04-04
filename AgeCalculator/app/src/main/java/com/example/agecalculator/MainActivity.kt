be089c008a9dc6684bf7466f21f3bd807c2f42e4
RashikAnsar <sk.ansar46@gmail.com>
2020-04-04 18:33:03 +0530

    day 4 [Age calculator in Days APP]

--- AgeCalculator/app/src/main/java/com/example/agecalculator/MainActivity.kt
+++ AgeCalculator/app/src/main/java/com/example/agecalculator/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.example.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            view -> clickDatePicker(view)
+        }
+    }
+
+    fun clickDatePicker(view: View) {
+        val myCalendar = Calendar.getInstance()
+
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        /**
+         * Creates a new date picker dialog for the specified date using the parent
+         * context's default date picker dialog theme.
+         */
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+
+                tvSelectedDate.setText(selectedDate)
+
+                var sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
+                val selectedDateInMinutes = theDate!!.time / 60000
+
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateToMinutes = currentDate!!.time / 60000
+
+                val differenceInDays = (currentDateToMinutes - selectedDateInMinutes) / (60 * 24)
+
+                tvSelectedDateInDays.setText(differenceInDays.toString())
+
+            }, year, month, day)
+
+        // 86400000 is milliseconds of 24 Hours. Which is used to restrict the user to select today and future day.
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
     }
 }
