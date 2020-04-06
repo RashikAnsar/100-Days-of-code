b4862f33510259b202c09467a8d7be594a6799f4
RashikAnsar <sk.ansar46@gmail.com>
2020-04-06 10:32:41 +0530

    Day 06 Quiz App welcome screen ui

--- Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
+++ Quiz/app/src/main/java/com/example/quiz/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
