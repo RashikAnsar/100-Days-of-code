34106cad21181e4277c7d94d39f2b014db1a4daf
RashikAnsar <sk.ansar46@gmail.com>
2020-04-09 22:41:21 +0530

    Day 09 Undo Funcitionality

--- WhiteBoard/app/src/main/java/com/example/whiteboard/MainActivity.kt
+++ WhiteBoard/app/src/main/java/com/example/whiteboard/MainActivity.kt
@@ -51,6 +51,10 @@ class MainActivity : AppCompatActivity() {
                 requestStoragePermission()
             }
         }
+
+        ib_undo.setOnClickListener {
+            drawing_view.onClickUndo()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
